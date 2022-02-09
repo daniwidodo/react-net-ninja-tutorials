4cccd584d4295d9c49f00a370ebd48465aee6870
bangtur_studio <dani.widodo87@gmail.com>
2022-02-09 14:01:28 +0800

    finish part 19

--- src/Home.js
+++ src/Home.js
@@ -5,24 +5,48 @@ const Home = () => {
   // let name = 'mario';
 
   const [blogs, setBlogs] = useState( null );
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
    console.log("from Home component :", blogs);
 
   useEffect(() => {
     // console.log("use Effect running", name);
-    fetch('http://localhost:8000/blogs')
-    .then(res => {
-      return res.json();
-    })
-    .then( data => {
-      console.log(data);
-      setBlogs(data);
-    });
+
+    setTimeout( () => {
+      //
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        // console.log(res);
+        // if OK
+        if (!res.ok) 
+        {
+          throw Error('gak bisa ambil data!');
+        }
+        //
+        return res.json();
+      })
+      .then( data => {
+        console.log(data);
+        setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch( err => {
+        setIsPending(false);
+        setError(err.message);
+        console.log(err.message);
+      })
+
+    }, 1000 )
+   
 
   }, []);
 
   return (
     <div className="home">
+      { error && <div> gak bisa ambil data </div> }
+      { isPending && <div>loading ...</div> }
       { blogs && <BlogList blogs={blogs} title="All blogs"  />}
      
      
