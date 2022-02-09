9b10ee1564080f3c18699932c4febdc6234507e3
bangtur_studio <dani.widodo87@gmail.com>
2022-02-09 14:02:19 +0800

    rapi - rapi

--- src/Home.js
+++ src/Home.js
@@ -4,52 +4,48 @@ import BlogList from "./BlogList";
 const Home = () => {
   // let name = 'mario';
 
-  const [blogs, setBlogs] = useState( null );
+  // STATE //
+  const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
 
-   console.log("from Home component :", blogs);
+  // LOGS //
+  console.log("from Home component :", blogs);
 
   useEffect(() => {
     // console.log("use Effect running", name);
 
-    setTimeout( () => {
+    setTimeout(() => {
       //
-      fetch('http://localhost:8000/blogs')
-      .then(res => {
-        // console.log(res);
-        // if OK
-        if (!res.ok) 
-        {
-          throw Error('gak bisa ambil data!');
-        }
-        //
-        return res.json();
-      })
-      .then( data => {
-        console.log(data);
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch( err => {
-        setIsPending(false);
-        setError(err.message);
-        console.log(err.message);
-      })
-
-    }, 1000 )
-   
-
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          // console.log(res);
+          // if OK
+          if (!res.ok) {
+            throw Error("gak bisa ambil data!");
+          }
+          //
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+          console.log(err.message);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
-      { error && <div> gak bisa ambil data </div> }
-      { isPending && <div>loading ...</div> }
-      { blogs && <BlogList blogs={blogs} title="All blogs"  />}
-     
-     
+      {error && <div> gak bisa ambil data </div>}
+      {isPending && <div>loading ...</div>}
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
 };
